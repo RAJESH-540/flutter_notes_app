b7cb2790aa98637628cad381819bce0d8343f3fe
RAJESH-540 <rkrajesh540@gmail.com>
2023-12-09 20:11:37 +0530

    add database with update delete or add data

---
+++ lib/data/db_helper.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+import 'package:notes_app/models/notes_model.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DbHelper {
+  DbHelper._();
+
+  static final DbHelper instance = DbHelper._();
+  Database? _database;
+  static const note_table = "note";
+  static const note_id = "note_id";
+  static const note_title = "note_title";
+  static const note_desc = "note_desc";
+
+  Future<Database> getDb() async {
+    if (_database != null) {
+      return _database!;
+    } else {
+      return await initDb();
+    }
+  }
+
+  Future<Database> initDb() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    var dbpath = join(directory.path + "notesdb.db");
+    return openDatabase(dbpath, version: 1, onCreate: (db, version) {
+      db.execute(
+          "create table $note_table ($note_id integer primary key autoincrement,$note_title text,$note_desc text)");
+    });
+  }
+
+  insertData(NotesModel notesModel) async {
+    var db = await getDb();
+    db.insert(note_table, notesModel.toMap());
+  }
+   Future<List<NotesModel>> getData() async{
+     var db = await getDb();
+     List<NotesModel>listNotes=[];
+     var data = await db.query(note_table);
+     for(Map<String, dynamic>eachData in data){
+        NotesModel notesModel=NotesModel.fromMap(eachData);
+        listNotes.add(notesModel);
+
+     }
+      return listNotes;
+
+   }
+    Future<void> updateNotes( NotesModel notesModel)async{
+     var db= await getDb();
+      db.update(note_table, notesModel.toMap(), where: "$note_id= ${notesModel.id}");
+    }
+
+     Future<bool> deleteNotes(int id)async{
+     var db= await getDb();
+      var count =  await db.delete(note_table, where: "$note_id=?", whereArgs: [id.toString()]);
+       return count>0;
+     }
+
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/custom_widget/circle_button.dart';
 import 'package:notes_app/custom_widget/radiotile.dart';
+import 'package:notes_app/custom_widget/textfield.dart';
+import 'package:notes_app/data/db_helper.dart';
+import 'package:notes_app/models/notes_model.dart';
 
 class NotesHomeScreen extends StatefulWidget {
   const NotesHomeScreen({super.key});
@@ -10,157 +13,152 @@ class NotesHomeScreen extends StatefulWidget {
 }
 
 class _NotesHomeScreenState extends State<NotesHomeScreen> {
+  var titleController = TextEditingController();
+  var descController = TextEditingController();
+  late DbHelper db;
+  List<NotesModel> data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    db = DbHelper.instance;
+    getAppData();
+  }
+
+  getAppData() async {
+    data = await db.getData();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return   SafeArea(
+    return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.black,
-        body: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      CircleAvatar(
-                        radius: 25,
-                        backgroundImage: NetworkImage("https://t4.ftcdn.net/jpg/05/60/70/71/360_F_560707155_hwQ7Kzf5fic1eF2VMWTgZvBFlzXOihVA.jpg",
-                        ),
-                      ),
-                      SizedBox(width: 5,),
-                      Text(
-                        "Hi Rajesh",
-                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Color(0xff1A1A1A),
-                        radius: 25,
-                        child: Icon(Icons.menu, color: Colors.white,),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 20,),
-              Text("My Notes",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold ,
-                    fontSize: 42),),
-              SizedBox(height: 10,),
-              Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  CircleContainer(text: "All (20)", bgColor: Colors.white,textColor: Colors.black,),
-                  CircleContainer(text: "Important",textColor: Colors.grey,),
-                  CircleContainer(text: "BookMarked",textColor: Colors.grey,),
-                ],
-              ),
-               SizedBox(height: 30,),
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                    height: 220,
-                    width: 170,
-                    decoration: BoxDecoration(
-                       color: Color(0xffFCA590),
-                      borderRadius: BorderRadius.circular(24)
-                        
-                    ),
-                    child: Center(
-
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Buy Honey\n100% original", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-                          Text("Buy the new brand\nhoney for my family \nhere's is the pic"),
-                           Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                             children: [
-                               Image.asset("assets/images/viha.webp",
-                               fit: BoxFit.contain,height: 80,
-                               width: 100,),
-                             ],
-                           )
-
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 220,
-                    width: 170,
-                    // padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                    decoration: BoxDecoration(
-                       color: Color(0xffFFFFA0),
-                        borderRadius: BorderRadius.circular(24)
-                    ),
-                     child: const Center(
-                       child: Padding(
-                         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                         child: Column(
-                           crossAxisAlignment: CrossAxisAlignment.start,
-                           mainAxisAlignment: MainAxisAlignment.center,
-                           children: [
-                             Text("Plane for the\ntoday", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-                             SizedBox(height: 5,),
-                             RadioTile(text: "text", title: "Buy Food",),
-                             SizedBox(height: 5,),
-                             RadioTile(text: "text", title: "GYM",),
-                             SizedBox(height: 5,),
-                             RadioTile(text: "text", title: "Meeting",)
-                           ],
-                         ),
-                       ),
-                     ),
-                  ),
-
-
-                ],
-              ),
-           SizedBox(height: 28,),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 26, vertical: 14),
-           margin: EdgeInsets.symmetric(horizontal: 12),
-          height: 150,
-          width: MediaQuery.of(context).size.width,
-           decoration: BoxDecoration(
-             color: Color(0xffCDEFF1),
-              borderRadius: BorderRadius.circular(24)
-           ),
-             child: Column(
-               mainAxisAlignment: MainAxisAlignment.start,
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 Text("Tax payment before the end  of march", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-                 Text(
-                     "This is remanider note, so as  not to forget to pay\ntaxes before the end of march. Don't miss it.you could be fined.\nList of assets must be reported"),
-
-
-               ],
-             ),
-         
-          )
-
-            ],
-          ),
+        resizeToAvoidBottomInset: true,
+        // backgroundColor: Colors.black,
+        appBar: AppBar(
+          elevation: 2,
+          title: Text("Notes app"),
+          centerTitle: true,
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _showBottomsheet();
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: ListView.builder(
+            itemCount: data.length,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: (){
+                  titleController.text=data[index].title;
+                  descController.text=data[index].desc;
+                   showModalBottomSheet(context: context, builder: (BuildContext context){
+                      return Container(
+                        height: 400,
+                        width: 400,
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+
+                        ),
+                        child: Center(
+                          child: SingleChildScrollView(
+                            child: Column(
+                               mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                CustomTextField(hint: "title", controller: titleController),
+                                CustomTextField(hint: "desc", controller: descController),
+                                const SizedBox(height: 20,),
+                                ElevatedButton(onPressed: (){
+                                  var utitle=titleController.text.toString();
+                                  var udesc=descController.text.toString();
+                                  db.updateNotes(NotesModel(title: utitle, desc: udesc, id: data[index].id));
+                                   getAppData();
+                                    Navigator.pop(context);
+                                }, child: Text("Update"))
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                   });
+                },
+                child: ListTile(
+
+                  leading: Text("${data[index].id}",
+                    style: const TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                  title: Text(data[index].title.toString(),
+                    style: const TextStyle(color: Colors.black,fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(data[index].desc.toString(),
+                    style: const TextStyle(color: Colors.black),
+                  ),
+                  trailing: IconButton(onPressed: (){
+                     db.deleteNotes(data[index].id!);
+                      getAppData();
+                  }, icon:  const Icon(Icons.delete)),
+                ),
+              );
+            }),
       ),
     );
+  }
+
+  _showBottomsheet() {
+    return showModalBottomSheet(
+        context: context,
+        builder: (BuildContext buildContext) {
+          return Container(
+            height: 400.0,
+            width: 400.0,
+            padding: const EdgeInsets.all(16),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(36), topRight: Radius.circular(36)),
+            ),
+            child: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CustomTextField(
+                        hint: " Enter a name", controller: titleController),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    CustomTextField(
+                        hint: " Enter desc", controller: descController),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            maximumSize: const Size(300, 50),
+                            minimumSize: const Size(300, 50),
+                            backgroundColor: Colors.deepPurple,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            )),
+                        onPressed: () async {
+                         await db.insertData(NotesModel(
+                              title: titleController.text.toString(),
+                              desc: descController.text.toString()));
+                          await getAppData();
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Save"))
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
   }
 }
